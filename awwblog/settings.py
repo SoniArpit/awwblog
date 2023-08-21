d2845611298e00681caa3c681f40a2fa603e3715
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-08-21 22:19:18 +0300

    Remove unused imports

--- awwblog/settings.py
+++ awwblog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'django_ckeditor_5',
+    'ckeditor',
     'ckeditor_uploader',
     'taggit',
     'django.contrib.sites',
