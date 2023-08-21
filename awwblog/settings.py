7df75687799e2187167a8b2897aaa3f2f3de2013
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-08-21 21:31:36 +0300

    Add project requirements

--- awwblog/settings.py
+++ awwblog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'ckeditor', 
+    'django_ckeditor_5',
     'ckeditor_uploader',
     'taggit',
     'django.contrib.sites',
